804839395436ff744028921d61c40bbeb0954ea0
liuq <liuq@fxiaoke.com>
2016-08-12 17:47:11 +0800

    add temp path for yozo

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/convertor/ConvertorHelper.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/convertor/ConvertorHelper.java
@@ -1,17 +1,23 @@
 package com.facishare.document.preview.cgi.convertor;
 
 import application.dcs.Convert;
+import com.github.autoconf.spring.reloadable.ReloadableProperty;
 
 /**
  * Created by liuq on 16/8/8.
  */
 public class ConvertorHelper {
-
-    public static  int  doConvert(String in,String out) {
+    @ReloadableProperty("temp-dir")
+    private static String tempDir="";
+    public static int doConvert(String in, String out) {
         ConvertorObject convertorObject = getConvertor();
         Convert convertor = convertorObject.getConvertor();
-        int code = in.toLowerCase().contains(".pdf")?convertor.convertPdfToHtml(in,out):convertor.convertMStoHtmlOfSvg(in, out);
-        convertorObject.getConvertor().deleteTempFiles();
+        convertor.setAcceptTracks(true);
+        convertor.setConvertForPhone(true);
+        convertor.setHtmlEncoding("UTF-8");
+        convertor.setTempPath(tempDir);
+        int code = in.toLowerCase().contains(".pdf") ? convertor.convertPdfToHtml(in, out) : convertor.convertMStoHtmlOfSvg(in, out);
+        convertor.deleteTempFiles();
         ConvertorPool.getInstance().releaseConvertor(convertorObject);
         return code;
     }
@@ -20,10 +26,5 @@ public class ConvertorHelper {
         ConvertorObject convertorObject = ConvertorPool.getInstance().getConvertor();
         convertorObject.getConvertor();
         return convertorObject;
-    }
-
-    private static void closeConvertor(ConvertorObject convertorObject) {
-        convertorObject.getConvertor().deleteTempFiles();
-        ConvertorPool.getInstance().releaseConvertor(convertorObject);
     }
 }

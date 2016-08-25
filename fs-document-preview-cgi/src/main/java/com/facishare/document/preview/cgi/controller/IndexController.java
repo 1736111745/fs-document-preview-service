0a2014fca656df5c4e1142954c0e0b8433f9b864
liuq <liuq@fxiaoke.com>
2016-08-25 16:27:51 +0800

    code..

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/IndexController.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/IndexController.java
@@ -1,12 +1,9 @@
 package com.facishare.document.preview.cgi.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,13 +21,8 @@ import java.util.Properties;
 @Controller
 @RequestMapping("/")
 public class IndexController{
-    private static final Logger LOG = LoggerFactory.getLogger(PreviewController.class);
     @RequestMapping(method = RequestMethod.GET)
     public void index(HttpServletResponse response) throws IOException {
-        Cookie cookie = new Cookie("FSAuthX", "0G60AEUkLW4000343w3bRA0g1CKFthRDMVZyxb2xk8RPaToBhzJa2L0pTyY3Hr7UbO8rHEefF6mPARxQdPwYR2eqWSQ2O1DCUekJzGsICDAJYB45Hw0l4VTzuzzKgiYPserHWmxF0wZmEpQOyojVxgSFubVPbOeJTu9NgrJJtkRFOrF0FMUO1zRAZNdhDWLcUWjTkJ5fYr22zYqctWeiyYtwhAIhsrszP36JTwAMxRo7G97wpP4g2WvmHj13");
-        cookie.setMaxAge(24 * 60 * 60 * 30);
-        response.addCookie(cookie);
-
         PrintWriter printWriter=response.getWriter();
         Properties props=System.getProperties(); //系统属性
         List<String> inputArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
@@ -53,7 +45,7 @@ public class IndexController{
         printWriter.println("Java Home: "+props.getProperty("java.home"));
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         printWriter.println();
-        printWriter.println("Time:"+df.format(new Date()));// new Date()为获取当前系统时间
+        printWriter.println("Time:"+df.format(new Date()));
     }
 
 }

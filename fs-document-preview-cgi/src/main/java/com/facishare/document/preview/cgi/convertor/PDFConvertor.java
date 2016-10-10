94699ebb6d7b11f5b2c2a35fb5cc07095b9c6985
liuq <liuq@fxiaoke.com>
2016-10-10 19:22:58 +0800

    code..

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/convertor/PDFConvertor.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/convertor/PDFConvertor.java
@@ -32,7 +32,7 @@ public class PDFConvertor implements IDocConvertor {
                 ipicConvertor.close();
                 File file = new File(pngFilePath);
                 if (file.exists()) {
-                    Thumbnails.of(file).scale(0.5).outputFormat("png").toFile(file);
+                    Thumbnails.of(file).size(414,736).outputFormat("png").toFile(file);
                     return FilenameUtils.getBaseName(baseDir) + "/" + fileName;
                 } else {
                     return "";

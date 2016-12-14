f4a3103936ad46802d15479ca9f28ba306918063
liuq <liuq@fxiaoke.com>
2016-12-14 10:36:21 +0800

    fix bug

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/service/PreviewService.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/service/PreviewService.java
@@ -1,11 +1,16 @@
 package com.facishare.document.preview.cgi.service;
 
+import com.alibaba.fastjson.JSON;
 import com.facishare.document.preview.cgi.dao.PreviewInfoDao;
-import com.facishare.document.preview.cgi.model.*;
+import com.facishare.document.preview.cgi.model.EmployeeInfo;
+import com.facishare.document.preview.cgi.model.PageInfo;
+import com.facishare.document.preview.cgi.model.PreviewInfo;
+import com.facishare.document.preview.cgi.model.PreviewInfoEx;
 import com.facishare.document.preview.cgi.utils.DocPageInfoHelper;
 import com.facishare.document.preview.cgi.utils.FileStorageProxy;
 import com.facishare.document.preview.cgi.utils.PathHelper;
 import com.facishare.document.preview.cgi.utils.SampleUUID;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,7 @@ import java.util.List;
 /**
  * Created by wuzh on 2016/11/23.
  */
+@Slf4j
 @Service
 public class PreviewService {
     @Autowired
@@ -26,6 +32,7 @@ public class PreviewService {
 
     /**
      * 手机预览
+     *
      * @param employeeInfo
      * @param path
      * @param securityGroup
@@ -33,40 +40,42 @@ public class PreviewService {
      * @throws Exception
      */
     public PreviewInfoEx getPreviewInfo(EmployeeInfo employeeInfo, String path, String securityGroup) throws Exception {
+        log.info("employeeInfo:{},path:{},securityGroup:{}",JSON.toJSONString(employeeInfo),path,securityGroup);
         String ea = employeeInfo.getEa();
         int employeeId = employeeInfo.getEmployeeId();
-        PreviewInfoEx previewInfoEx=new PreviewInfoEx();
-        PreviewInfo previewInfo = previewInfoDao.getInfoByPath(ea, path);
-        int pageCount;
-        List<String> sheetNames;
-        if (previewInfo == null) {
-            byte[] bytes = fileStorageProxy.GetBytesByPath(path, employeeInfo, securityGroup);
-            if (bytes != null && bytes.length > 0) {
-                String extension = FilenameUtils.getExtension(path).toLowerCase();
-                String dataDir = new PathHelper(ea).getDataDir();
-                String fileName = SampleUUID.getUUID() + "." + extension;
-                String filePath = FilenameUtils.concat(dataDir, fileName);
-                //下载下来保存便于文档转换方便 // TODO: 2016/11/10 当所有的页码都转码完毕后需要删除.
-                FileUtils.writeByteArrayToFile(new File(filePath), bytes);
-                PageInfo pageInfo = DocPageInfoHelper.GetPageInfo(bytes, filePath);
-                if (pageInfo.isSuccess()) {
-                    pageCount = pageInfo.getPageCount();
-                    sheetNames = pageInfo.getSheetNames();
-                    previewInfo = previewInfoDao.initPreviewInfo(ea, employeeId, path, filePath, dataDir, bytes.length, pageCount, sheetNames);
-                    previewInfoEx.setSuccess(true);
-                    previewInfoEx.setPreviewInfo(previewInfo);
+        PreviewInfoEx previewInfoEx = new PreviewInfoEx();
+        try {
+            PreviewInfo previewInfo = previewInfoDao.getInfoByPath(ea, path);
+            int pageCount;
+            List<String> sheetNames;
+            if (previewInfo == null) {
+                byte[] bytes = fileStorageProxy.GetBytesByPath(path, employeeInfo, securityGroup);
+                if (bytes != null && bytes.length > 0) {
+                    String extension = FilenameUtils.getExtension(path).toLowerCase();
+                    String dataDir = new PathHelper(ea).getDataDir();
+                    String fileName = SampleUUID.getUUID() + "." + extension;
+                    String filePath = FilenameUtils.concat(dataDir, fileName);
+                    //下载下来保存便于文档转换方便 // TODO: 2016/11/10 当所有的页码都转码完毕后需要删除.
+                    FileUtils.writeByteArrayToFile(new File(filePath), bytes);
+                    PageInfo pageInfo = DocPageInfoHelper.GetPageInfo(bytes, filePath);
+                    if (pageInfo.isSuccess()) {
+                        pageCount = pageInfo.getPageCount();
+                        sheetNames = pageInfo.getSheetNames();
+                        previewInfo = previewInfoDao.initPreviewInfo(ea, employeeId, path, filePath, dataDir, bytes.length, pageCount, sheetNames);
+                        previewInfoEx.setSuccess(true);
+                        previewInfoEx.setPreviewInfo(previewInfo);
+                    } else {
+                        previewInfoEx.setSuccess(false);
+                        previewInfoEx.setPreviewInfo(null);
+                        previewInfoEx.setErrorMsg(pageInfo.getErrorMsg());
+                    }
                 }
-                else {
-                    previewInfoEx.setSuccess(false);
-                    previewInfoEx.setPreviewInfo(null);
-                    previewInfoEx.setErrorMsg(pageInfo.getErrorMsg());
-                }
+            } else {
+                previewInfoEx.setSuccess(true);
+                previewInfoEx.setPreviewInfo(previewInfo);
             }
-        }
-        else
-        {
-            previewInfoEx.setSuccess(true);
-            previewInfoEx.setPreviewInfo(previewInfo);
+        } catch (Exception e) {
+            log.error("getPreviewInfo happend exception!", e);
         }
         return previewInfoEx;
     }

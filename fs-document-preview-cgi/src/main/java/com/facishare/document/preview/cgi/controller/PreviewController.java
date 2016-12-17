896ffe2cce0757f958b43bf8939efc134e352c34
liuq <liuq@fxiaoke.com>
2016-12-18 01:12:30 +0800

    add some log

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/PreviewController.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/PreviewController.java
@@ -121,7 +121,9 @@ public class PreviewController {
                 } else {
                     String originalFilePath = previewInfo.getOriginalFilePath();
                     ConvertDocArg convertDocArg = ConvertDocArg.builder().originalFilePath(originalFilePath).page(pageIndex).path(path).build();
+                    log.info("begin do convert,arg:{}",convertDocArg);
                     ConvertDocResult convertDocResult = docConvertService.convertDoc(convertDocArg);
+                    log.info("end do convert,result:{}",convertDocResult);
                     dataFilePath = convertDocResult.getDataFilePath();
                     if (!Strings.isNullOrEmpty(dataFilePath)) {
                         fileOutPutor.outPut(response, dataFilePath);

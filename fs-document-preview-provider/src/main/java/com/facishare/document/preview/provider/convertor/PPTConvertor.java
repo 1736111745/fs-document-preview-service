df8996e8d75169d56ee26424d8ebdfcc187c9a67
liuq <liuq@fxiaoke.com>
2016-12-20 12:56:46 +0800

    调整代码结构

--- fs-document-preview-provider/src/main/java/com/facishare/document/preview/provider/convertor/PPTConvertor.java
+++ fs-document-preview-provider/src/main/java/com/facishare/document/preview/provider/convertor/PPTConvertor.java
@@ -1,7 +1,5 @@
 package com.facishare.document.preview.provider.convertor;
 
-import com.google.common.base.Strings;
-import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 

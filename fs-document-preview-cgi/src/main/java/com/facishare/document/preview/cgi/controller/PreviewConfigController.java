1238ba97c9965392728c2d6b8e7245f40d866cee
liuq <liuq@fxiaoke.com>
2016-12-28 15:32:44 +0800

    修改log

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/PreviewConfigController.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/PreviewConfigController.java
@@ -5,6 +5,9 @@ import com.facishare.document.preview.cgi.model.EmployeeInfo;
 import com.facishare.document.preview.cgi.model.PreviewWayEntity;
 import com.fxiaoke.release.FsGrayRelease;
 import com.fxiaoke.release.FsGrayReleaseBiz;
+import com.github.autoconf.spring.reloadable.ReloadableProperty;
+import com.google.common.base.Strings;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +22,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/")
 public class PreviewConfigController {
     private FsGrayReleaseBiz gray = FsGrayRelease.getInstance("dps");
+    @ReloadableProperty("allowPreviewExtension")
+    private String allowPreviewExtension = "doc|docx|xls|xlsx|ppt|pptx|pdf";
     @ResponseBody
     @RequestMapping(value = "/preview/getPreviewConfig", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public String getPreviewWay(HttpServletRequest request) {
@@ -28,7 +33,15 @@ public class PreviewConfigController {
         EmployeeInfo employeeInfo = (EmployeeInfo) request.getAttribute("Auth");
         String user = "E." + employeeInfo.getEa() + "." + employeeInfo.getEmployeeId();
         boolean newway = gray.isAllow(grayConfig, user);
-        if (newway) {
+        String path=safteGetRequestParameter(request,"path");
+        boolean useNewWay=true;
+        if(!Strings.isNullOrEmpty(path)) {
+            String extension = FilenameUtils.getExtension(path).toLowerCase();
+            if (allowPreviewExtension.indexOf(extension) == -1) {
+                useNewWay = false;
+            }
+        }
+        if (newway&&useNewWay) {
             entity.setWay(1);
             String byTokenUrl = "/dps/preview/bytoken?token={0}&name={1}";
             String byPathUrl = "/dps/preview/bypath?path={0}&name={1}";

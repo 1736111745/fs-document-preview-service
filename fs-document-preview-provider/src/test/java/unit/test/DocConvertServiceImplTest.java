339f10d4433ce704b1fb123cb9dd211aebcb0275
liuq <liuq@fxiaoke.com>
2017-01-20 13:52:44 +0800

    add test case

---
+++ fs-document-preview-provider/src/test/java/unit/test/DocConvertServiceImplTest.java
@@ -0,0 +1,45 @@
+package unit.test;
+
+import com.facishare.document.preview.provider.service.DocConvertServiceImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * DocConvertServiceImpl Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>十二月 20, 2016</pre>
+ */
+public class DocConvertServiceImplTest {
+    @Autowired
+    DocConvertServiceImpl docConvertService;
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: getPageInfo(GetPageInfoArg arg)
+     */
+    @Test
+    public void testGetPageInfo() throws Exception {
+
+    }
+
+    /**
+     * Method: convertDoc(ConvertDocArg arg)
+     */
+    @Test
+    public void testConvertDoc() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 

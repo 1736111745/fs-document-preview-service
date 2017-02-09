c7d44a32d6dec27d74a7084b8015bab2b533e667
liuq <liuq@fxiaoke.com>
2017-02-09 11:40:12 +0800

    修复word预览可复制的问题

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/word_ppt.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/word_ppt.js
@@ -1,17 +1,21 @@
 var sg=getQueryStringByName("sg");
-function loadData() {
-    var pageCount = getQueryStringByName("pageCount");
-    var path = getQueryStringByName("path");
-    for (var i = 0; i < pageCount; i++) {
-        var src = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount+"&sg="+sg;
-        var page = $("<div class='content'><embed class='lazy' data-original='" + src + "' height='30%' width='100%'/></div>");
-        $("#divPages").append(page);
-        var nav = $("<div class='center'><span>第" + (i + 1) + "页,共" + pageCount + "页</span></div>");
-        page.append(nav);
+var pageCount = getQueryStringByName("pageCount");
+var path = getQueryStringByName("path");
+$(function() {
+    $(window).bind('scroll',function(){show()});
+    var i = 1;
+    function show()
+    {
+        if($(window).scrollTop()+$(window).height()>=$(document).height()*0.75)
+        {
+            i++;
+            if(i<pageCount){
+                var src = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount+"&sg="+sg;
+                var page = $("<div class='content'><embed class='lazy' src='" + src + "' width='100%' height='100%' type='image/svg+xml'/></div>");
+                $("#divPages").append(page);
+                var nav = $("<div class='center'><span>第" + (i + 1) + "页,共" + pageCount + "页</span></div>");
+                page.append(nav);
+            }
+        }
     }
-    $("embed.lazy").lazyload();
-}
-//入口
-$(function () {
-    loadData();
 });

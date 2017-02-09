58e86be1113e29b82ea915b6b5fb632249ba26e0
liuq <liuq@fxiaoke.com>
2017-02-09 11:48:10 +0800

    修复word预览可复制的问题

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/word_ppt.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/word_ppt.js
@@ -1,21 +1,34 @@
 var sg=getQueryStringByName("sg");
 var pageCount = getQueryStringByName("pageCount");
 var path = getQueryStringByName("path");
+var end=pageCount>3?3:pageCount;
 $(function() {
+    loadPrev3Page();
+    var __start__=pageCount-end;
     $(window).bind('scroll',function(){show()});
-    var i = 1;
     function show()
     {
         if($(window).scrollTop()+$(window).height()>=$(document).height()*0.75)
         {
-            i++;
-            if(i<pageCount){
-                var src = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount+"&sg="+sg;
-                var page = $("<div class='content'><embed class='lazy' src='" + src + "' width='100%' height='100%' type='image/svg+xml'/></div>");
-                $("#divPages").append(page);
-                var nav = $("<div class='center'><span>第" + (i + 1) + "页,共" + pageCount + "页</span></div>");
-                page.append(nav);
+            __start__++;
+            if(__start__<pageCount){
+                loadData(__start__);
             }
         }
+    }
+    
+    function loadPrev3Page() {
+        for (var i = 0; i < end; i++) {
+            loadData(i);
+        }
+    }
+
+
+    function loadData(i) {
+        var src = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount+"&sg="+sg;
+        var page = $("<div class='content'><embed class='lazy' src='" + src + "' width='100%' height='100%' type='image/svg+xml'/></div>");
+        $("#divPages").append(page);
+        var nav = $("<div class='center'><span>第" + (i + 1) + "页,共" + pageCount + "页</span></div>");
+        page.append(nav);
     }
 });

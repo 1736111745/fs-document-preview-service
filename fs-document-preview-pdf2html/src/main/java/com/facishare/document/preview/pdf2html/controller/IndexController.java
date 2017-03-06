ab61d84ee07db58ea198402ef0d8d190cf231191
liuq <liuq@fxiaoke.com>
2017-03-06 17:57:49 +0800

    fix bug

--- fs-document-preview-pdf2html/src/main/java/com/facishare/document/preview/pdf2html/controller/IndexController.java
+++ fs-document-preview-pdf2html/src/main/java/com/facishare/document/preview/pdf2html/controller/IndexController.java
@@ -1,9 +1,6 @@
 package com.facishare.document.preview.pdf2html.controller;
 
-import com.facishare.document.preview.api.model.arg.Pdf2HtmlArg;
-import com.facishare.document.preview.pdf2html.service.Pdf2HtmlServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 @Slf4j
 public class IndexController {
-    @Autowired
-    Pdf2HtmlServiceImpl pdf2HtmlService;
     @RequestMapping(method = RequestMethod.GET)
     public String index() throws Exception {
         return "index";

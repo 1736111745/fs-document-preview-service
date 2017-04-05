958ba9060a269775f57b478a340fea78e37b7026
liuq <34310374@qq.com>
2017-04-05 13:35:33 +0800

    fix bug

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/OnlineOfficeServerUtil.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/OnlineOfficeServerUtil.java
@@ -1,8 +1,10 @@
 package com.facishare.document.preview.cgi.utils;
 
+import com.facishare.document.preview.common.utils.SampleUUID;
 import com.fxiaoke.common.http.handler.SyncCallback;
 import com.fxiaoke.common.http.spring.OkHttpSupport;
 import com.github.autoconf.ConfigFactory;
+import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -13,6 +15,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.net.URLEncoder;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by liuq on 2017/3/29.
@@ -34,35 +37,35 @@ public class OnlineOfficeServerUtil {
     }
 
     public byte[] downloadPdfFile(String ea, int employeeId, String path, String sg) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         String ext = FilenameUtils.getExtension(path).toLowerCase();
-        String name = "考勤记录表（3月1日 - 3月31日）" + "." + ext;
+        String name = SampleUUID.getUUID() + "." + ext;
         String downloadUrl = ext.contains("ppt") ? generateDownloadUrlForPPT(ea, employeeId, path, sg, name)
                 : generateDownloadUrlForWordAndPdf(ea, employeeId, path, sg, name);
-        log.info("begin down load file from oos,url:{}", downloadUrl);
+        log.info("begin download file from oos,url:{}", downloadUrl);
         Request request = new Request.Builder().url(downloadUrl).header("Connection", "close").build();
         Object object = client.syncExecute(request, new SyncCallback() {
             @Override
             public Object response(Response response) {
                 try {
                     return response.body().bytes();
-                }
-                catch (Exception e)
-                {
-                    log.warn("exception:",e);
-                    return  null;
-                }
-                finally {
-                    log.info("response.status:{}",response.code());
+                } catch (Exception e) {
+                    log.warn("exception:", e);
+                    return null;
+                } finally {
+                    log.info("response.status:{}", response.code());
                 }
             }
         });
+        stopwatch.stop();
+        log.info("download completed!cost:{}", stopwatch.elapsed(TimeUnit.MILLISECONDS));
         return (byte[]) object;
     }
 
     private String generateDownloadUrlForWordAndPdf(String ea, int employeeId, String path, String sg, String name) {
         String downloadUrl = String.format(fscServerUrl, ea, String.valueOf(employeeId), path, sg, name);
         String src = oosServerUrl + "/oh/wopi/files/@/wFileId?wFileId=" + URLEncoder.encode(downloadUrl);
-        String postUrl = oosServerUrl + "/wv/WordViewer/request.pdf?WOPIsrc=" + URLEncoder.encode(src) + "&type=downloadpdf";
+        String postUrl = oosServerUrl + "/wv/WordViewer/request.pdf?WOPIsrc=" + URLEncoder.encode(src) + "&type=accesspdf";
         return postUrl;
     }
 

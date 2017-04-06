e0c873889858a29aa156e791412bed32f27d8f4d
liuq <34310374@qq.com>
2017-04-06 16:06:23 +0800

    增加office2pdf模块

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/UrlParametersHelper.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/UrlParametersHelper.java
@@ -1,21 +1,20 @@
 package com.facishare.document.preview.cgi.utils;
 
 import com.google.common.base.Strings;
-import lombok.experimental.UtilityClass;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by liuq on 2017/4/6.
  */
-@UtilityClass
+
 public class UrlParametersHelper {
-    public String safeGetRequestParameter(HttpServletRequest request, String paramName) {
+    public static String safeGetRequestParameter(HttpServletRequest request, String paramName) {
         String value = request.getParameter(paramName) == null ? "" : request.getParameter(paramName).trim();
         return value;
     }
 
-    public  boolean isValidPath(String path) {
+    public static   boolean isValidPath(String path) {
         if (Strings.isNullOrEmpty(path))
             return false;
         if (path.startsWith("N_") || path.startsWith("TN_") || path.startsWith("TG_")

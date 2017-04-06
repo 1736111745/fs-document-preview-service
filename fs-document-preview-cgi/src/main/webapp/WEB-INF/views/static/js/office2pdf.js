d313720c90ef23c3247c27bb9d73c54884dba9bb
liuq <34310374@qq.com>
2017-04-06 18:08:19 +0800

    增加office2pdf模块

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
@@ -3,29 +3,27 @@ var pageCount = getQueryStringByName("pageCount");
 var path = getQueryStringByName("path");
 var type = path.indexOf(".doc") > -1 ? "checkWord2Pdf" : "checkPPT2Pdf"
 var tryCount = 0;
+var url = window.contextPath + '/preview/' + type + '?path=' + path + "&sg=" + sg
 $(function () {
     checkOffice2Pdf();
 });
-function checkOffice2Pdf() {
-    var url = window.contextPath + '/preview/' + type + '?path=' + path + "&sg=" + sg
-    var call = function () {
-        if(tryCount++>10) {
-            return;
-        }
-        $.ajax({
-            type: 'get',
-            dataType: 'json',
-            async: true,
-            url: url,
-            success: function (data) {
-                if (data.finished) {
-                    location.href = window.contextPath + '/preview/handlePdf?path=' + path + '&pageCount=' + pageCount + "&sg=" + sg;
-                }
-                else
-                {
-                    call();
-                }
-            }
-        });
+var checkOffice2Pdf = function () {
+    if(tryCount++>10) {
+        return;
     }
+    $.ajax({
+        type: 'get',
+        dataType: 'json',
+        async: true,
+        url: url,
+        success: function (data) {
+            if (data.finished) {
+                location.href = window.contextPath + '/preview/handlePdf?path=' + path + '&pageCount=' + pageCount + "&sg=" + sg;
+            }
+            else
+            {
+                checkOffice2Pdf();
+            }
+        }
+    });
 }

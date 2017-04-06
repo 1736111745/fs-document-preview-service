a2ab1b566fbe3e4c0503fd031762b94b2d39947b
liuq <34310374@qq.com>
2017-04-06 17:55:22 +0800

    增加office2pdf模块

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
@@ -6,18 +6,6 @@ var tryCount = 0;
 $(function () {
     checkOffice2Pdf();
 });
-//定时监测转换状态，当全部转换完毕停止检测
-// function checkPPT2PdfStatus() {
-//     tryCount++;
-//     idChkOffice2Pdf = setInterval(function () {
-//         checkOffice2Pdf();
-//         if (finished || tryCount > 10) {
-//             clearInterval(idChkOffice2Pdf);
-//
-//         }
-//     }, 2000);
-// }
-
 function checkOffice2Pdf() {
     var url = window.contextPath + '/preview/' + type + '?path=' + path + "&sg=" + sg
     var call = function () {

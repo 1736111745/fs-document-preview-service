426982bef4d7667495d49cc1e89473523af3bb0a
liuq <34310374@qq.com>
2017-04-07 22:25:10 +0800

    修改老的预览页面加载方式

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/office2pdf.js
@@ -3,9 +3,17 @@ var pageCount = getQueryStringByName("pageCount");
 var path = getQueryStringByName("path");
 var type = path.indexOf(".doc") > -1 ? "checkWord2Pdf" : "checkPPT2Pdf"
 var tryCount = 0;
+var finished = false
 $(function () {
+    loadViewPort();
     checkOffice2Pdf();
 });
+function loadViewPort() {
+    var docWidth = $(window).width();
+    var scale = docWidth * 0.96 / 1000;
+    var viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute('content', 'initial-scale=' + scale + ', width=device-width');
+}
 var checkOffice2Pdf = function () {
     console.log("try count:" + tryCount);
     var url = window.contextPath + '/preview/' + type + '?path=' + path + "&sg=" + sg
@@ -22,13 +30,15 @@ var checkOffice2Pdf = function () {
             if (status == "success") {
                 var data = XMLHttpRequest.responseJSON;
                 if (data.finished) {
+                    finished = true;
                     location.href = window.contextPath + '/preview/handlePdf?path=' + path + '&pageCount=' + pageCount + "&sg=" + sg;
                 }
             }
             if (status == 'timeout') {
                 console.log("time out!")
             }
-            checkOffice2Pdf();
+            if (!finished)
+                checkOffice2Pdf();
         }
     });
 }

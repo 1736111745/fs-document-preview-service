4a3be8b8e6f40b600cc1a2cd3cccfdd15d1810c6
liuq <34310374@qq.com>
2017-04-16 02:12:49 +0800

    fix bug

--- fs-document-preview-common/src/main/java/com/facishare/document/preview/common/utils/Office2PdfApiHelper.java
+++ fs-document-preview-common/src/main/java/com/facishare/document/preview/common/utils/Office2PdfApiHelper.java
@@ -2,11 +2,11 @@ package com.facishare.document.preview.common.utils;
 
 import com.facishare.document.preview.common.model.RestResponse;
 import com.fxiaoke.common.http.handler.SyncCallback;
-import com.fxiaoke.common.http.spring.OkHttpSupport;
 import com.github.autoconf.ConfigFactory;
 import com.github.autoconf.spring.reloadable.ReloadableProperty;
 import com.google.common.base.Stopwatch;
 import com.google.common.base.Strings;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.apache.commons.io.FilenameUtils;
@@ -15,7 +15,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -29,12 +28,18 @@ public class Office2PdfApiHelper {
 
     @ReloadableProperty("oosServerUrl")
     private String oosServerUrl = "";
-    @Resource(name = "httpClientSupport")
-    private OkHttpSupport client;
+    private static OkHttpClient client = null;
 
     @PostConstruct
     void init() {
         ConfigFactory.getConfig("fs-dps-config", config -> oosServerUrl = config.get("oosServerUrl"));
+    }
+
+    static {
+        client = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
     }
 
     public int getPageCount(String filePath) throws IOException {
@@ -68,20 +73,24 @@ public class Office2PdfApiHelper {
         }
         Request request = new Request.Builder().url(postUrl).post(requestBody).build();
         try {
-            client.syncExecute(request, new SyncCallback() {
+            client.newCall(request).enqueue(new Callback() {
                 @Override
-                public Object response(Response response) throws Exception {
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
                     String contentType = response.header("Content-Type");
                     log.info("contentType:{}", contentType);
                     log.info("response.status:{}", response.code());
                     byte[] bytes = response.body().bytes();
                     restResponse.setBytes(bytes);
                     restResponse.setContentType(contentType);
-                    return bytes;
                 }
             });
         } catch (Exception ex) {
-            log.error("call method:{},path:{},happened exception!",method, filePath, ex);
+            log.error("call method:{},path:{},happened exception!", method, filePath, ex);
         }
         stopwatch.stop();
         log.info("call api:{},cost:{}ms", postUrl, stopwatch.elapsed(TimeUnit.MILLISECONDS));

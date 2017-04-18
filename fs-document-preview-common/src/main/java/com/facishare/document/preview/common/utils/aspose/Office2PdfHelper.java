ed31b7e8dff8a91ab87fe7a6eb143e0ec6c67afb
liuq <34310374@qq.com>
2017-04-18 11:59:23 +0800

    fix bug

--- fs-document-preview-common/src/main/java/com/facishare/document/preview/common/utils/aspose/Office2PdfHelper.java
+++ fs-document-preview-common/src/main/java/com/facishare/document/preview/common/utils/aspose/Office2PdfHelper.java
@@ -2,7 +2,9 @@ package com.facishare.document.preview.common.utils.aspose;
 
 
 import com.aspose.words.PdfSaveOptions;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -10,16 +12,20 @@ import java.io.File;
 /**
  * Created by liuq on 2017/4/18.
  */
+@Component
+@Slf4j
 public class Office2PdfHelper {
     public Office2PdfHelper() {
         LicenceHelper.setAllLicence();
     }
 
     public byte[] convertWord2Pdf(String filePath) throws Exception {
+        log.info("begin convert word to pdf!");
         com.aspose.words.Document document = new com.aspose.words.Document(filePath);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PdfSaveOptions pdfSaveOptions = new PdfSaveOptions();
         document.save(outputStream, pdfSaveOptions);
+        log.info("end convert word to pdf,file size:{},cost:{}ms",FileUtils.sizeOf(new File(filePath)));
         return outputStream.toByteArray();
     }
 

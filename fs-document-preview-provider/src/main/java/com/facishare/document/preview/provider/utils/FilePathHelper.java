1f095455f33b8a9cf95f493c40cf0b4264a00619
liuq <34310374@qq.com>
2017-04-20 11:37:05 +0800

    fix bug

--- fs-document-preview-provider/src/main/java/com/facishare/document/preview/provider/utils/FilePathHelper.java
+++ fs-document-preview-provider/src/main/java/com/facishare/document/preview/provider/utils/FilePathHelper.java
@@ -8,14 +8,9 @@ import java.io.File;
  * Created by liuq on 2016/12/20.
  */
 public class FilePathHelper {
-    public static String getFilePath(String filePath, int startPageIndex, int startIndex, String fileExt) {
-        String baseDir = FilenameUtils.getFullPathNoEndSeparator(filePath);
-        String fileDirPath=FilenameUtils.concat(baseDir, "p" +  (startPageIndex + startIndex));
-        File fileDir=new File(fileDirPath);
-        if(!fileDir.exists()) {
-            fileDir.mkdir();
-        }
-        String fileName = (startPageIndex + startIndex) + "." + fileExt;
-        return FilenameUtils.concat(baseDir, fileDirPath + "/" + fileName);
-    }
+  public static String getFilePath(String filePath,int startPageIndex,int startIndex,String fileExt) {
+      String baseDir = FilenameUtils.getFullPathNoEndSeparator(filePath);
+      String fileName = (startPageIndex + startIndex) + "." + fileExt;
+      return FilenameUtils.concat(baseDir, fileName);
+  }
 }

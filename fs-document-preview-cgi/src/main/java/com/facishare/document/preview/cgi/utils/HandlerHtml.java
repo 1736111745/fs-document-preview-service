c9c84cafc0bf0c66d46bb931cf1c56ba1d46436e
liuq <34310374@qq.com>
2017-05-15 23:19:22 +0800

    fix bug

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/HandlerHtml.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/utils/HandlerHtml.java
@@ -24,7 +24,7 @@ public class HandlerHtml {
             String encoding = "UTF-8";
             File htmlFile = new File(filePath);
             long fileSize = htmlFile.length();
-            log.info("begin handle html,filePath:{},page:{},file size", filePath, page, fileSize);
+            log.info("begin handle html,filePath:{},page:{},file size:{}", filePath, page, fileSize);
             if (fileSize > 1024 * 1024 * 10) {
                 //如果生成的html大于10m，就不给个错误提示
                 String html = "<h3>该工作表数据异常，请检查工作表的行数或者列数是否过大！</h3>";

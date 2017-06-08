2b9a2ecc940d859bea43953df8fedc5a20c631a2
dongxiaohua <dongxiaohua@fxiaoke.com>
2017-06-08 19:23:27 +0800

    编写接口清理预览文件夹及mongo中对应的数据

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/RestfulController.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/controller/RestfulController.java
@@ -3,12 +3,18 @@ package com.facishare.document.preview.cgi.controller;
 import com.facishare.document.preview.cgi.model.EmployeeInfo;
 import com.facishare.document.preview.cgi.model.PreviewInfoEx;
 import com.facishare.document.preview.cgi.service.PreviewService;
+import com.facishare.document.preview.common.dao.PreviewInfoDao;
+import com.facishare.document.preview.common.model.PreviewInfo;
 import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.http.HttpClient;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/restful")
@@ -16,7 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestfulController {
     @Autowired
     private PreviewService previewService;
-
+    @Autowired
+    PreviewInfoDao previewInfoDao;
     /**
      * 获取文档页码数
      *
@@ -42,6 +49,33 @@ public class RestfulController {
         return ret;
     }
 
+    /**
+     *
+     * @param filePath 文件路径
+     * @param ea  企业账号
+     * @return
+     */
+    @RequestMapping(value = "/maintance/preview/clean", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public boolean documentPageClean(String filePath, String ea) {
+       //todo:1.删除monogo记录 2.清理文件。
+
+        //从mongo中读取逻辑路径（记录）
+        PreviewInfo previewInfo=previewInfoDao.getInfoByPath(ea,filePath);
+        if (previewInfo == null) {
+            //previewInfoDao.clean(ea , filePath);
+            return true;
+        }
+        //获取文件夹实际路径
+        String fileDir=previewInfo.getDataDir();
+        File file = new File(fileDir);
+        //递归删除，预防文件夹中还存在文件夹
+        FileUtils.deleteQuietly(file);
+        //删除mongo数据
+        previewInfoDao.clean(ea , filePath);
+        log.info("删除成功");
+        return true;
+
+    }
     public PreviewInfoEx getPreviewInfo(EmployeeInfo employeeInfo, String path) throws Exception {
         return previewService.getPreviewInfo(employeeInfo, path,"");
     }

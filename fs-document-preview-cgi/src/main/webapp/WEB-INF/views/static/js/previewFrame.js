b5a36bd89e7cfe1c3f9a66a1d030c119c3895d9f
liuq <34310374@qq.com>
2017-08-01 17:57:49 +0800

    fix bug

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/previewFrame.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/previewFrame.js
@@ -4,11 +4,11 @@
 var sg = getQueryStringByName("sg");
 var pageCount = getQueryStringByName("pageCount");
 var path = getQueryStringByName("path");
-var page=getQueryStringByName("page");
+var page = getQueryStringByName("page");
 
 $(function () {
-   loadViewPort();
-   loadData(page);
+  loadViewPort();
+  loadData(page);
   $(window).resize(function () {
     loadViewPort();
   });
@@ -24,12 +24,16 @@ function loadViewPort() {
 
 function loadData(i) {
   var url = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount + "&sg=" + sg + "&ver=2.1";
-  var iframe="<iframe id='framePage' src='" + url + "' onload='resize(this)' onresize='resize(this)' scrolling='no' frameborder='0' width='100%'></iframe>";
+  var iframe = "<iframe id='framePage' src='" + url + "' onload='resize(this)' onresize='resize(this)' scrolling='no' frameborder='0' width='100%'></iframe>";
   $('#main').html(iframe);
 }
 
 function resize(obj) {
-  var height = $(obj.contentWindow.document).find("div[id='page-container']").height()
-  $(obj).height(height + 20);
-  $(obj.parentElement).removeClass("lazy");
+  var height = $(obj.contentWindow.document).find("div[id='page-container']").height();
+  var iframeHeight = height + 20;
+  $(obj).height(iframeHeight);
+  var docHeight = $(window).height();
+  var top = (docHeight - iframeHeight) / 2;
+  $("#main").css("margin-top", top + "px");
+
 }

cd4d61585f4da8ac669c8843c3f6eb301c8f33a4
liuq <34310374@qq.com>
2017-08-16 10:58:05 +0800

    fix bug

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/filter/AuthFilter.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/filter/AuthFilter.java
@@ -35,7 +35,7 @@ public class AuthFilter extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request,
                                   HttpServletResponse response,
                                   FilterChain filterChain) throws ServletException, IOException {
-    request.setAttribute("sv", "v5.1.8");
+    request.setAttribute("sv", "v5.1.9");
     String requestUri = request.getRequestURI().toLowerCase();
     if (ignoreAuth(request)) {
       filterChain.doFilter(request, response);

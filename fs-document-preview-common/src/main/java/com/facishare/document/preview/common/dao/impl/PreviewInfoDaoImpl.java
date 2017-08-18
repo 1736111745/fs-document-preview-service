f0ef4e6b50eb26f3a1ec8af9270b15dc779c7b3c
liuq <34310374@qq.com>
2017-08-18 17:36:25 +0800

    add some log

--- fs-document-preview-common/src/main/java/com/facishare/document/preview/common/dao/impl/PreviewInfoDaoImpl.java
+++ fs-document-preview-common/src/main/java/com/facishare/document/preview/common/dao/impl/PreviewInfoDaoImpl.java
@@ -171,10 +171,10 @@ public class PreviewInfoDaoImpl implements PreviewInfoDao {
     if (width == 1000) {
       if (path.startsWith("A_")) {
         query.and(query.criteria("path").equal(path))
-             .and(query.criteria("width").doesNotExist().or(query.criteria("width").equal(width)));
+             .and(query.or(query.criteria("width").doesNotExist()).or(query.criteria("width").equal(width)));
       } else {
-        query.and(query.criteria("path").equal(path).criteria("ea").equal("ea"))
-             .and(query.criteria("width").doesNotExist().or(query.criteria("width").equal(width)));
+        query.and(query.criteria("path").equal(path).criteria("ea").equal(ea))
+             .and(query.or(query.criteria("width").doesNotExist()).or(query.criteria("width").equal(width)));
       }
     } else {
       if (path.startsWith("A_")) {

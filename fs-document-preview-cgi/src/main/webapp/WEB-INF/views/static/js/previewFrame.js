a699e99ab8ef9f729258ac7ce67249b4c9b90486
liuq <34310374@qq.com>
2017-09-07 10:28:45 +0800

    fix bug

--- fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/previewFrame.js
+++ fs-document-preview-cgi/src/main/webapp/WEB-INF/views/static/js/previewFrame.js
@@ -5,11 +5,11 @@ var sg = getQueryStringByName("sg");
 var pageCount = getQueryStringByName("pageCount");
 var path = getQueryStringByName("path");
 var page = getQueryStringByName("page");
-var screenWidth = window.screen.width;//屏幕宽度
-var screenHeight = window.screen.height;//屏幕高度；
+var screenWidth = window.screen.availWidth;//屏幕宽度
+var screenHeight = window.screen.availHeight;//屏幕高度；
 var ua = navigator.userAgent;
 var isIOS = /iphone|ipod|ipad/ig.test(ua);
-var width=1000;
+var width = 1000;
 $(function () {
   loadData(page);
   loadViewPort();
@@ -17,12 +17,12 @@ $(function () {
     //alert("isIOS:"+isIOS);
     if (isIOS) {
       if (window.orientation != 0) {//横屏 宽度取大的值
-        screenWidth = Math.max(window.screen.width, window.screen.height);
-        screenHeight = Math.min(window.screen.width, window.screen.height);
+        screenWidth = Math.max(window.screen.availWidth, window.screen.availHeight);
+        screenHeight = Math.min(window.screen.availWidth, window.screen.availHeight);
       }
       else {//竖屏 宽度取小得值
-        screenWidth = Math.min(window.screen.width, window.screen.height);
-        screenHeight = Math.max(window.screen.width, window.screen.height);
+        screenWidth = Math.min(window.screen.availWidth, window.screen.availHeight);
+        screenHeight = Math.max(window.screen.availWidth, window.screen.availHeight);
       }
     }
     else {
@@ -31,7 +31,7 @@ $(function () {
     }
     window.setTimeout(function () {
       loadViewPort();
-    },200);
+    }, 200);
   }, true);
 })
 
@@ -48,7 +48,7 @@ function loadViewPort() {
 
 
 function loadData(i) {
-  var url = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount + "&width="+width+"&sg=" + sg + "&ver=2.1";
+  var url = window.contextPath + '/preview/getFilePath?path=' + path + '&page=' + i + "&pageCount=" + pageCount + "&width=" + width + "&sg=" + sg + "&ver=2.1";
   var iframe = "<iframe id='framePage' src='" + url + "' onload='resize(this)' onresize='resize(this)' scrolling='no' frameborder='0' width='100%'></iframe>";
   $('#main').html(iframe);
 }

702c6a4f1c603134be68ef8a1bd94516ac1b1508
liuq <liuq@fxiaoke.com>
2017-12-07 11:42:58 +0800

    fix

--- fs-document-preview-asyncconvertor/src/main/java/com/facishare/document/preview/asyncconvertor/utils/Pdf2HtmlHandler.java
+++ fs-document-preview-asyncconvertor/src/main/java/com/facishare/document/preview/asyncconvertor/utils/Pdf2HtmlHandler.java
@@ -29,8 +29,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
@@ -142,7 +140,7 @@ public class Pdf2HtmlHandler {
     args.add("--dest-dir");//输出目录
     args.add(outPutDir);
     args.add(filePath);
-    //log.info("args:{}", args);
+    log.info("args:{}", args);
     return args;
   }
 

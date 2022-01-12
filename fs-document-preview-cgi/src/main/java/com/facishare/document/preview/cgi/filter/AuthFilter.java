d913c97f8fc18f3f413d218255a37fd2182bedc7
刘泉 <>
2022-01-12 10:45:59 +0800

    AuthFilter取消请求资源的白名单设置

--- fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/filter/AuthFilter.java
+++ fs-document-preview-cgi/src/main/java/com/facishare/document/preview/cgi/filter/AuthFilter.java
@@ -33,17 +33,14 @@ public class AuthFilter extends OncePerRequestFilter {
   @ReloadableProperty("authTempKey")
   private String authTempKey = "~]Ec5SrXX<.557uf";
   @ReloadableProperty("staticFileVersion")
-  private String staticFileVersion="v5.3.30";
+  private String staticFileVersion = "v5.3.30";
 
   private @Context
   HttpServletRequest request;
 
 
-
   @Override
-  protected void doFilterInternal(HttpServletRequest request,
-                                  HttpServletResponse response,
-                                  FilterChain filterChain) throws ServletException, IOException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     request.setAttribute("sv", staticFileVersion);
     String requestUri = request.getRequestURI().toLowerCase();
     if (ignoreAuth(request)) {
@@ -97,7 +94,8 @@ public class AuthFilter extends OncePerRequestFilter {
               request.setAttribute("Auth", employeeInfo);
               //设置下cookie
               //String authCookieStr="FSAuthX=0G60A8NRRG400012qn7OLB6uLrqNwPyFvqaIbS7hPSz5bEUo2JrvReHzbzL1xzKoxWY2odJxfy8060ILdxEFfVlqzbhjz4RS3bzdyltFmviyrJLWdcuj6z2lscxq2QKzycZwAM8jDxjxkOS7paFZ85CbJy7eeiorJfCgjwdp9nboGWIhGFZIW7jKeaAqeILjv0azXdklRbRUH6cNCHDFvSr2MP8bY7p90hpWHzT";
-              Cookie  authCookie=new Cookie("FSAuthX","0G60A8NRRG400012qn7OLB6uLrqNwPyFvqaIbS7hPSz5bEUo2JrvReHzbzL1xzKoxWY2odJxfy8060ILdxEFfVlqzbhjz4RS3bzdyltFmviyrJLWdcuj6z2lscxq2QKzycZwAM8jDxjxkOS7paFZ85CbJy7eeiorJfCgjwdp9nboGWIhGFZIW7jKeaAqeILjv0azXdklRbRUH6cNCHDFvSr2MP8bY7p90hpWHzT");
+              Cookie authCookie = new Cookie("FSAuthX",
+                "0G60A8NRRG400012qn7OLB6uLrqNwPyFvqaIbS7hPSz5bEUo2JrvReHzbzL1xzKoxWY2odJxfy8060ILdxEFfVlqzbhjz4RS3bzdyltFmviyrJLWdcuj6z2lscxq2QKzycZwAM8jDxjxkOS7paFZ85CbJy7eeiorJfCgjwdp9nboGWIhGFZIW7jKeaAqeILjv0azXdklRbRUH6cNCHDFvSr2MP8bY7p90hpWHzT");
               authCookie.setDomain("ceshi112.com");
               authCookie.setPath("/");
               response.addCookie(authCookie);
@@ -119,10 +117,8 @@ public class AuthFilter extends OncePerRequestFilter {
   private static boolean ignoreAuth(HttpServletRequest request) {
     String requestUri = request.getRequestURI().toLowerCase();
     String ctx = request.getContextPath();
-    if (requestUri.startsWith(ctx + "/open/") || requestUri.startsWith(ctx + "/restful/") ||
-      requestUri.startsWith(ctx + "/share/") || requestUri.equals(ctx + "/") || requestUri.contains(".js") ||
-      requestUri.contains(".svg") || requestUri.contains(".png") || requestUri.contains(".css") ||
-      requestUri.contains(".jpg") || requestUri.contains(".htm") || requestUri.contains("ping")) {
+    if (requestUri.startsWith(ctx + "/open/") || requestUri.startsWith(ctx + "/restful/") || requestUri.startsWith(ctx + "/share/") ||
+      requestUri.equals(ctx + "/") || requestUri.contains("ping")) {
       return true;
     } else {
       return false;
